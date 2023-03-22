9d221c0f3972d94caa32c6a2fb2596bff0579501
Vostrova Svetlana <masyanyamasyanya1@gmail.com>
2023-03-23 00:31:55 +0300

    Ñadd Task18

--- seminar_python_3/task_18.py
+++ seminar_python_3/task_18.py
@@ -1,6 +1,6 @@
-# Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N]. Пользователь в первой строке вводит
-# натуральное число N – количество элементов в массиве. В последующих  строках записаны N целых чисел Ai.
-# Последняя строка содержит число X
+# Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих строках записаны N целых чисел Ai. Последняя строка содержит число X.
 def check_input(arg1: str):
     return arg1.isdigit()
 
@@ -9,21 +9,30 @@ def convert_input(arg1: str):
     return int(arg1)
 
 
-def get_number_duplicates(arg1: list, arg2: str):
-    count = arg1.count(arg2)
-    return count
-
-
 if __name__ == "__main__":
     count_el = input("Введите количество чисел в массиве: ")
     if check_input(count_el):
         count_el = convert_input(count_el)
-        my_list = [input("Введите элемент массива: ") for el in range(count_el)]
+        my_list = []
+        for i in range(count_el):
+            user_input = input("Введите элемент массива: ")
+            if check_input(user_input):
+                my_list.append(convert_input(user_input))
+            else:
+                my_list.append(0)
+
         number = input("Введите число которое необходимо найти в списке: ")
         if not check_input(number):
             print("Введите число")
         else:
-            count_duplicates = get_number_duplicates(my_list, number)
-            print(f"Число {number} встречается в массиве {count_duplicates} раз")
+            number = convert_input(number)
+            min_diff = number - my_list[0]
+            index_number = 0
+            for i in range(1, count_el):
+                count = number - my_list[i]
+                if count < min_diff:
+                    min_diff = count
+                    index_number = i
+            print(f"В массиве число {my_list[index_number]} самый близкий по величине элемент к числу {number}")
     else:
         print("Нужно ввести число.")
