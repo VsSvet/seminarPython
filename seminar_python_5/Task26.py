198f49a9d97ce48ccbdff6237cb65d43e4f50c90
Vostrova Svetlana <masyanyamasyanya1@gmail.com>
2023-03-28 18:00:00 +0300

    add Task26

---
+++ seminar_python_5/Task26.py
@@ -0,0 +1,14 @@
+# Напишите программу, на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+def exponentiation(a, b):
+    if b > 0:
+        return a * exponentiation(a, b - 1)
+    return 1
+
+
+basis, indicator = input("Введите основание степени: "), input("Введите показатель степени: ")
+if basis.isdigit() and indicator.isdigit():
+    basis, indicator = int(basis), int(indicator)
+    result = exponentiation(basis, indicator)
+    print(result)
+else:
+    print("Нужно ввести число, а не строку")
