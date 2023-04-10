ed92dacf8e33612bd796adfed20bc75727d4c5d0
Vostrova Svetlana <masyanyamasyanya1@gmail.com>
2023-04-10 08:49:29 +0300

    фÐadd template

---
+++ seminar_python_8/phonebook.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+telephone_book = Path("seminar_python_8", "telephone_book.txt")
+
+
+class Contact:
+    def __init__(self):
+        pass
+
+    def get_contact(self):
+        pass
+
+
+class Phonebook:
+    def __init__(self):
+        pass
+
+    def add_contact(self):
+        pass
+
+    def find_contact(self):
+        pass
+
+    def edit_contact(self):
+        pass
+
+    def delete_contact(self):
+        pass
+
+if __name__ == '__main__':
+    pass
